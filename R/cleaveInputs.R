21e18378110c1c5f25b432509ae041cc5f262f8f
carlganz <carlganz@gmail.com>
2017-02-22 20:03:05 +0000

    add credit card and fix server side

--- R/cleaveInputs.R
+++ R/cleaveInputs.R
@@ -26,31 +26,54 @@ phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = N
   
 }
 
+#' @rdname phoneInput
+#' @export
+#'
+
+creditCardInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL) {
+  
+  shiny::tagList(
+    shiny::textInput(inputId, label, value, width, placeholder),
+    shiny::tags$script(
+      paste0('
+      var cleave = new Cleave("#', inputId, '", {
+        creditCard: true,
+        onCreditCardTypeChanged: function(type) {
+          Shiny.onInputChange("',inputId,'" + 
+            "_creditCard", type);
+        }
+      });
+    ')
+    )
+  )
+  
+}
+
 
 #' Server side functionality
 #' 
 #' Cleave inputs from the server
 #' 
-#' @param session The \code{session} object passed to function given to \code{shinyServer}
+#' @param session The `session` object passed to function given to `shinyServer`
 #' @param selector CSS Selector specifying the input/s
 #' @param inputType List of options passed to Cleave as JSON; see notes
 #' @export
 #' @note See \url{http://nosir.github.io/cleave.js/} for options
-#' @note Javascript functions passed to inputType should be wrapped in \code{I()}
+#' @note Javascript functions passed to inputType should be wrapped in `I()`
 #' @importFrom jsonlite toJSON
 #' @import shiny
 #' 
 
-cleave <- function(session, selector, inputType) {
+cleave <- function(session, selector, inputOptions) {
   
   options <- jsonlite::toJSON(
     list(
       selector = selector,
-      inputType = inputType
+      inputOptions = inputOptions
     ),
     auto_unbox = TRUE
   )
-  
+
   session$sendCustomMessage("cleave", options)
   
 }
--- inst/javascript/shinyCleave.js
+++ inst/javascript/shinyCleave.js
@@ -3,7 +3,7 @@
 */
 
 Shiny.addCustomMessageHandler("cleave", function(options) {
-  
+
   var cleave = new Cleave(options.selector, options.inputOptions);
   
 });
