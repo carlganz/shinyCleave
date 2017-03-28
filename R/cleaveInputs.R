3c5b1e131f9e28ba990797acd773096a66e191d9
carlganz <carlganz@gmail.com>
2017-03-28 11:26:50 -0700

    add arbitrary input

--- R/cleaveInputs.R
+++ R/cleaveInputs.R
@@ -10,10 +10,10 @@
 #' @export
 #'
 
-phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL, country = "us") {
+phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL, country = "us", ...) {
   
   shiny::tagList(
-    shiny::textInput(inputId, label, value, width, placeholder),
+    shinyCleave::myInput("text", inputId, label, value, width, placeholder, ...),
     shiny::tags$script(
       paste0('
       var cleave = new Cleave("#', inputId, '", {
@@ -23,6 +23,15 @@ phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = N
     ')
   )
   )
+  
+}
+
+#' @export
+#' @rdname phoneInput
+zipInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL, ...) {
+  
+  myInput("text",inputId, label, value, width, placeholder, maxlength = "5",
+          onkeypress='return event.charCode >= 48 && event.charCode <= 57', ...)
   
 }
 
@@ -53,4 +62,35 @@ cleave <- function(session, selector, inputType) {
   
   session$sendCustomMessage("cleave", options)
   
+}
+
+
+#' Shiny inputs with additional parameters
+#' 
+#' Allow user to use standard Shiny inputs, but with ... so additional attributes can be used
+#' 
+#' @inheritParams shiny::textInput
+#' @param type Type of Shiny input
+#' @param ... Additional attributes passed to Shiny input
+#' @export
+#' 
+myInput <- function(type, inputId, label, value = "", width = NULL, placeholder = NULL, class = "", ...) 
+{
+  value <- restoreInput(id = inputId, default = value)
+  class <- paste0("form-control ",class)
+  tagList(div(class = "form-group shiny-input-container", style = if (!is.null(width)) 
+            paste0("width: ", validateCssUnit(width), ";"), label %AND% 
+              tags$label(label, `for` = inputId), tags$input(id = inputId, 
+                                                             type = type, class = class, 
+                                                             value = value, 
+                                                             placeholder = placeholder,...)))
+}
+
+# copied from shiny
+`%AND%` <- function (x, y) 
+{
+  if (!is.null(x) && !is.na(x)) 
+    if (!is.null(y) && !is.na(y)) 
+      return(y)
+  return(NULL)
 }
